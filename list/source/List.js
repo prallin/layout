50fc4ec0d5cfe056de0cd86dda43e8cf656fe89d
patalano <chris.patalano@palm.com>
2012-11-27 15:10:20 -0800

    Working on JIRA ticket 1313 - Added support for swipeable items to list.
    Enyo-DCO-1.1-Signed-off-by: Chris Patalano <chris.patalano@palm.com>

--- list/source/List.js
+++ list/source/List.js
@@ -62,7 +62,11 @@ enyo.kind({
 		//* If true, the selected item will toggle
 		toggleSelected: false,
 		//* If true, the list will assume all rows have the same height for optimization
-		fixedHeight: false
+		fixedHeight: false,
+		
+		swipeableComponents: [],
+		enableSwipe: false,
+		persistSwipeableItem: false
 	},
 	events: {
 		/**
@@ -70,10 +74,19 @@ enyo.kind({
 			
 			_inEvent.index_ contains the current row index.
 		*/
-		onSetupItem: ""
+		onSetupItem: "",
+		onSwipeStart: "",
+		onSwipeDrag: "",
+		onSwipe: "",
+		onSwipeComplete: ""
 	},
 	handlers: {
-		onAnimateFinish: "animateFinish"
+		onAnimateFinish: "animateFinish",
+		ondragstart: "dragstart",
+        ondragfinish: "dragfinish",
+        ondrag: "drag",
+		onflick: "flick",
+		onwebkitTransitionEnd: "transitionComplete"
 	},
 	//* @protected
 	rowHeight: 0,
@@ -83,7 +96,8 @@ enyo.kind({
 				{tag: null, name: "client"}
 			]},
 			{name: "page0", allowHtml: true, classes: "enyo-list-page"},
-			{name: "page1", allowHtml: true, classes: "enyo-list-page"}
+			{name: "page1", allowHtml: true, classes: "enyo-list-page"},
+			{name: "swipeableComponents", style: "position:absolute;display:block;top:-1000px;left:0px;"}
 		]}
 	],
 	create: function() {
@@ -101,6 +115,15 @@ enyo.kind({
 		this.createChrome(this.listTools);
 		this.controlParentName = "client";
 		this.discoverControlParent();
+	},
+	initComponents: function() {
+		this.inherited(arguments);
+		this.createSwipeableComponents();
+	},
+	createSwipeableComponents: function() {
+		for(var i=0;i<this.swipeableComponents.length;i++) {
+			this.$.swipeableComponents.createComponent(this.swipeableComponents[i], {owner: this.owner});
+		}
 	},
 	rendered: function() {
 		this.inherited(arguments);
@@ -283,7 +306,7 @@ enyo.kind({
 		var h = this.pageToPosition(page);
 		// update the page
 		this.updateForPosition(h);
-		// call pageToPosition again and this time should return the right pos since the page info is populated
+		// call pageToPosition again and this time should return the g pos since the page info is populated
 		h = this.pageToPosition(page);
 		this.setScrollPosition(h);
 		if (page == this.p0 || page == this.p1) {
@@ -396,5 +419,355 @@ enyo.kind({
 	twiddle: function() {
 		var s = this.getStrategy();
 		enyo.call(s, "twiddle");
+	},
+	
+	
+	// index of swiped item
+	swipeIndex: null,
+	// direction of swipe
+	swipeDirection: null,
+	// is a persistent item currently persisting
+	persistentItemVisisble: false,
+	// side from which the persisting item came from
+	persistentItemOrigin: null,
+	// specify if swipe was completed
+	swipeComplete: false,
+	// timeout used to wait before completing swipe action
+	completeSwipeTimeout: null,
+	// time in MS to wait before completing swipe action
+	completeSwipeDelayMS: 150,
+	// time in seconds for normal swipe animation
+	normalSwipeSpeed: 0.2,
+	// time in seconds for fast swipe animation
+	fastSwipeSpeed: 0.1,
+	// flag to specify whether a flick event happened
+	flicked: false,
+	// percentage of a swipe needed to force complete the swipe
+	percentageDraggedThreshold: 0.2,
+	
+	/*
+		When a drag starts, get the direction of the drag as well as the index of the item
+		being dragged, and reset any pertinent values. Then kick off the swipe sequence.
+	*/
+	dragstart: function(inSender, inEvent) {
+		// if no swipeable components are defined, or vertical drag, don't do swipe actions
+		if(!this.hasSwipeableComponents() || inEvent.vertical) {
+			return this.preventDragPropagation;
+		}
+		
+		// save direction we are swiping
+		this.setSwipeDirection(inEvent.xDirection);
+		
+		// if we are waiting to complete a swipe, complete it
+		if(this.completeSwipeTimeout) {
+			this.completeSwipe(inEvent);
+		}
+		
+		// reset flicked flag
+		this.setFlicked(false);
+		// reset swipe complete flag
+		this.setSwipeComplete(false);
+		
+		// if user is dragging a different item than was dragged previously, hide all swipeables first
+		if(this.swipeIndexChanged(inEvent.index)) {
+			this.clearSwipeables();
+			this.setSwipeIndex(inEvent.index);
+		}
+		
+		// start swipe sequence only if we are not currently showing a persistent item
+		if(!this.persistentItemVisisble) {
+			this.startSwipe(inEvent);
+		}
+		
+		return this.preventDragPropagation;
+	},
+	/*
+		When a drag is in progress, update the position of the swipeable container based on
+		the ddx of the event.
+	*/
+	drag: function(inSender, inEvent) {
+		// if swiping is disabled, return early
+		if(!this.getEnableSwipe()) {
+			return this.preventDragPropagation;
+		}
+		
+		// if a persistent swipeableItem is still showing, handle it separately
+		if(this.persistentItemVisisble) {
+			this.dragPersistentItem(inEvent);
+			return this.preventDragPropagation;
+		}
+		
+		// apply new position
+		this.dragSwipeableComponents(this.calcNewDragPosition(inEvent.ddx));
+		
+		return this.preventDragPropagation;
+	},
+	/*
+		When the user flicks, complete the swipe.
+	*/
+	flick: function(inSender, inEvent) {
+		// if swiping is disabled, return early
+		if(!this.getEnableSwipe()) {
+			return this.preventDragPropagation;
+		}
+		
+		// if the flick was vertical, return early
+		if(Math.abs(inEvent.xVelocity) < Math.abs(inEvent.yVelocity)) {
+			return this.preventDragPropagation;
+		}
+		
+		// prevent the dragFinish event from breaking the flick
+		this.setFlicked(true);
+		
+		// if a persistent swipeableItem is still showing, slide it away or bounce it
+		if(this.persistentItemVisisble) {
+			this.flickPersistentItem(inEvent);
+			return this.preventDragPropagation;
+		}
+		
+		// do swipe
+		this.swipe(inEvent,this.normalSwipeSpeed);
+		
+		return this.preventDragPropagation;
+	},
+	/*
+		When the current drag completes, decide whether to complete the swipe based on
+		how far the user pulled the swipeable container. If a flick occurred, don't
+		process dragFinish.
+	*/
+	dragfinish: function(inSender, inEvent) {
+		// if swiping is disabled, return early
+		if(!this.getEnableSwipe()) {
+			return this.preventDragPropagation;
+		}
+		// if a flick happened, don't do dragFinish
+		if(this.wasFlicked()) {
+			return this.preventDragPropagation;
+		}
+		
+		// if a persistent swipeableItem is still showing, complete drag away or bounce
+		if(this.persistentItemVisisble) {
+			this.dragFinishPersistentItem(inEvent);
+		// otherwise if user dragged more than 20% of the width, complete the swipe. if not, back out.
+		} else {
+			if(this.calcPercentageDragged(inEvent.dx) > this.percentageDraggedThreshold) {
+				this.swipe(inEvent,this.fastSwipeSpeed);
+			} else {
+				this.backOutSwipe(inEvent);
+			}
+		}
+		
+		return this.preventDragPropagation;
+	},
+	
+	hasSwipeableComponents: function() {
+		return this.$.swipeableComponents.controls.length != 0;
+	},
+	// Position the swipeable components block at the current row
+	positionSwipeableContainer: function(index,xDirection) {
+		var node = this.$.generator.fetchRowNode(index);
+		if(!node) {
+			return;
+		}
+		var offset = this.getRelativeOffset(node, this.hasNode());
+		var dimensions = this.getDimensions(node);
+		var x = (xDirection == 1) ? -1*parseInt(dimensions.width) : parseInt(dimensions.width);
+		this.$.swipeableComponents.addStyles("top: "+offset.top+"px; -webkit-transform: translate3d("+x+"px,0,0); height: "+dimensions.height+"; width: "+dimensions.width);
+	},
+	// Get offset relative to a positioned ancestor node
+    getRelativeOffset: function (n, p) {
+        var ro = {top: 0, left: 0};
+        if (n !== p && n.parentNode) {
+            do {
+                ro.top += n.offsetTop || 0;
+                ro.left += n.offsetLeft || 0;
+                n = n.offsetParent;
+            } while (n && n !== p);
+        }
+        return ro;
+    },
+	// Get height and width dimensions of the given dom node
+	getDimensions: function(node) {
+		var style = getComputedStyle(node,null);
+		var h = style.getPropertyValue("height");
+		var w = style.getPropertyValue("width");
+		return {height: h, width: w};
+	},
+	setSwipeDirection: function(xDirection) {
+		this.swipeDirection = xDirection;
+	},
+	setFlicked: function(flicked) {
+		this.flicked = flicked;
+	},
+	wasFlicked: function() {
+		return this.flicked;
+	},
+	setSwipeComplete: function(complete) {
+		this.swipeComplete = complete;
+	},
+	swipeIndexChanged: function(index) {
+		return (this.swipeIndex === null) ? true : (index === undefined) ? false : (index !== this.swipeIndex);
+	},
+	setSwipeIndex: function(index) {
+		this.swipeIndex = (index === undefined) ? this.swipeIndex : index;
+	},
+	/*
+		Calculate new position for the swipeable container based on the user's drag action. Don't
+		allow the container to drag further than either edge.
+	*/
+	calcNewDragPosition: function(dx) {
+		var parentStyle = getComputedStyle(this.$.swipeableComponents.node);
+		var xPos = parseInt(parentStyle.webkitTransform.split(",")[4]);
+		var dimensions = this.getDimensions(this.$.swipeableComponents.node);
+		var xlimit = (this.swipeDirection == 1) ? -1*parseInt(dimensions.width) : parseInt(dimensions.width);
+		var x = (this.swipeDirection == 1)
+			? (xPos + dx < xlimit)
+				? xlimit
+				: xPos + dx
+			: (xPos + dx > xlimit)
+				? xlimit
+				: xPos + dx;
+		return x;
+	},
+	dragSwipeableComponents: function(x) {
+		this.$.swipeableComponents.applyStyle("-webkit-transform","translate3d("+x+"px,0,0)");
+	},
+	// Begin swiping sequence by positioning the swipeable container and bubbling the setupSwipeItem event
+	startSwipe: function(e) {
+		this.positionSwipeableContainer(this.swipeIndex,e.xDirection);
+		this.$.swipeableComponents.setShowing(true);
+		this.setPersistentItemOrigin(e.xDirection);
+		this.doSwipeStart(e);
+	},
+	// if a persistent swipeableItem is still showing, drag it away or bounce it
+	dragPersistentItem: function(e) {
+		var xPos = 0;
+		var x = (this.persistentItemOrigin == "right")
+			? Math.max(xPos, (xPos + e.dx))
+			: Math.min(xPos, (xPos + e.dx));
+		this.$.swipeableComponents.applyStyle("-webkit-transform","translate3d("+x+"px,0,0)");
+	},
+	// if a persistent swipeableItem is still showing, complete drag away or bounce
+	dragFinishPersistentItem: function(e) {
+		var completeSwipe = (this.calcPercentageDragged(e.dx) > 0.2);
+		var dir = (e.dx > 0) ? "right" : (e.dx < 0) ? "left" : null;
+		if(this.persistentItemOrigin == dir) {
+			if(completeSwipe) {
+				this.slideAwayItem();
+			} else {
+				this.bounceItem(e);
+			}
+		} else {
+			this.bounceItem(e);
+		}
+	},
+	// if a persistent swipeableItem is still showing, slide it away or bounce it
+	flickPersistentItem: function(e) {
+		if(e.xVelocity > 0) {
+			if(this.persistentItemOrigin == "left") {
+				this.bounceItem(e);
+			} else {
+				this.slideAwayItem();
+			}
+		} else if(e.xVelocity < 0) {
+			if(this.persistentItemOrigin == "right") {
+				this.bounceItem(e);
+			} else {
+				this.slideAwayItem();
+			}
+		}
+	},
+	setPersistentItemOrigin: function(xDirection) {
+		this.persistentItemOrigin = xDirection == 1 ? "left" : "right";
+	},
+	calcPercentageDragged: function(dx) {
+		return Math.abs(dx/parseInt(getComputedStyle(this.$.swipeableComponents.node).width));
+	},
+	swipe: function(e,speed) {
+		this.setSwipeComplete(true);
+		this.swipeItem(0,speed,e);
+	},
+	backOutSwipe: function(e) {
+		var dimensions = this.getDimensions(this.$.swipeableComponents.node);
+		var x = (this.swipeDirection == 1) ? -1*parseInt(dimensions.width) : parseInt(dimensions.width);
+		this.swipeItem(x,0.1,e);
+	},
+	swipeItem: function(x,secs,e) {
+		var $item = this.$.swipeableComponents;
+		$item.applyStyle("-webkit-transition", "-webkit-transform "+secs+"s linear 0s");
+		$item.applyStyle("-webkit-transform","translate3d("+x+"px,0,0)");
+	},
+	bounceItem: function(e) {
+		if(parseInt(getComputedStyle(this.$.swipeableComponents.node).webkitTransform.split(",")[4]) != 0) {
+			this.swipeItem(0,this.normalSwipeSpeedSecs,e);
+		}
+	},
+	slideAwayItem: function() {
+		var $item = this.$.swipeableComponents;
+		var parentStyle = getComputedStyle($item.node);
+		var xPos = (this.persistentItemOrigin == "right") ? parseInt(parentStyle.width) : -1*parseInt(parentStyle.width);
+		$item.applyStyle("-webkit-transition", "-webkit-transform "+this.normalSwipeSpeedSecs+"s linear 0s");
+		$item.applyStyle("-webkit-transform","translate3d("+xPos+"px,0,0)");
+		this.persistentItemVisisble = false;
+		this.setPersistSwipeableItem(false);
+	},
+	resetSwipeableTransitionTime: function($item) {
+		this.$.swipeableComponents.applyStyle("-webkit-transition", "-webkit-transform 0s linear 0s");
+	},
+	clearSwipeables: function() {
+		this.$.swipeableComponents.setShowing(false);
+		this.persistentItemVisisble = false;
+		this.setPersistSwipeableItem(false);
+	},
+	/*
+		When the CSS transition for the swipeable animation has completed, wait for a defined timeout
+		before processing the swipe completion.
+	*/
+	transitionComplete: function(inSender, inEvent) {
+		if(inEvent.originator.name !== "swipeableComponents") {
+			return;
+		}
+		var _this = this;
+		this.completeSwipeTimeout = setTimeout(function() { _this.completeSwipe(inEvent); }, this.completeSwipeDelayMS);
+	},
+	// complete swipe and hide active swipeable item
+	completeSwipe: function(e) {
+		
+		if(this.completeSwipeTimeout) {
+			clearTimeout(this.completeSwipeTimeout);
+			this.completeSwipeTimeout = null;
+		}
+		
+		e.xDirection = this.swipeDirection;
+		
+		if(e.index === undefined) {
+			e.index = this.swipeIndex;
+		}
+		
+		this.setSwipeDirection(null);
+		this.resetSwipeableTransitionTime();
+		
+		// if this wasn't a persistent item, hide it upon completion and send swipe complete event
+		if(!this.getPersistSwipeableItem()) {
+			this.$.swipeableComponents.setShowing(false);
+			// if the swipe was completed, update the current row and bubble swipeComplete event
+			if(this.swipeComplete) {
+				this.doSwipeComplete(e);
+			}
+		} else {
+			this.persistentItemVisisble = true;
+		}
+	},
+	updateCurrentRow: function() {
+		// prepare row
+		this.prepareRow(this.swipeIndex);
+		// update row
+		this.renderRow(this.swipeIndex);
+		// lock it up
+		this.lockRow(this.swipeIndex);
 	}
 });
+
+
+
+
